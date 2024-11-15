5b022d509948b6d276818fccf2057b9fd3bb4ee8
EstudosWeb <148809403+estudoswebs@users.noreply.github.com>
2024-11-14 21:37:21 -0300

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+    // Your web app's Firebase configuration
+    const firebaseConfig = {
+        apiKey: "AIzaSyBhZyy8FRAJP_gHyrNFwaAyKfos_26IUI4",
+        authDomain: "notas-f9a71.firebaseapp.com",
+        projectId: "notas-f9a71",
+        storageBucket: "notas-f9a71.firebasestorage.app",
+        messagingSenderId: "54462767931",
+        appId: "1:54462767931:web:08cdb73e8511373263cd1e"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
